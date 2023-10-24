b56a2ed59fb1770876a879cebd9169eb16e05347
BuildTools <unconfigured@null.spigotmc.org>
2023-10-24 15:48:59 +0200

    Test 3

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Jebać stare baby prądem')
+    print_hi('Kurwa w koncu dziala')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
