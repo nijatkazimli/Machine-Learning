71f423cc20ff7e103c0a8e07e74d071b96b1c4fa
mikolaj-t <mikolaj.t@outlook.com>
2023-11-21 22:33:26 +0100

    project structure

---
+++ interfaces.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class FaceRecognitionModel(ABC):
+    @abstractmethod
+    def detect_faces(self, image):
+        pass
+
+class FaceRecognitionHandler(ABC):
+    def __init__(self, model: FaceRecognitionModel):
+        self.model = model
+
+    @abstractmethod
+    def handle_image(self, image_path, width, height):
+        pass
+
+    @abstractmethod
+    def handle_camera(self, cap):
+        pass
+
+    @abstractmethod
+    def handle_video(self, video_path, width, height):
+        pass
+
+class GUI(ABC):
+    def __init__(self, handler: FaceRecognitionHandler):
+        self.handler = handler
+
+    @abstractmethod
+    def show(self):
+        pass
