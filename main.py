e7de24eb4d97d40be1e0124e2cc55bbb5ea01bb7
mikolaj-t <mikolaj.t@outlook.com>
2023-11-21 23:54:05 +0100

    this is not really better, but merge to one file

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from models.face_recognition.haar_fast import HaarFastModel
+from models.face_recognition.haar import HaarFastModel, HaarSlowModel
 from models.face_recognition.handler import OpenCVFaceRecognition
 from app.gui import GUIImplementation
 
 def main():
-    model = HaarFastModel()
+    model = HaarSlowModel()
     face_recognition = OpenCVFaceRecognition(model)
 
     gui = GUIImplementation(face_recognition)
     gui.show()
 
 if __name__ == "__main__":
-    main()
