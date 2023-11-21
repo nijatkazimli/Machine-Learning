b1feebc3fc30382a6325978f9819e4cd73318818
mikolaj-t <mikolaj.t@outlook.com>
2023-11-21 23:12:30 +0100

    put everything together in main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from models.face_recognition.haar_fast import HaarFaceModel
+from models.face_recognition.handler import OpenCVFaceRecognition
+from app.gui import GUIImplementation
+
 def main():
-    pass
+    model = HaarFaceModel()
+    face_recognition = OpenCVFaceRecognition(model)
+
+    gui = GUIImplementation(face_recognition)
+    gui.show()  # Start the GUI
 
 if __name__ == "__main__":
     main()
