e81ba62139df758712865a71e6d18aa47dc299d6
Michał Bober <michalbober.lublin@gmail.com>
2023-12-29 18:27:53 +0100

    Fixed problems with tests precission added F1 score

--- tests/test_face_rec.py
+++ tests/test_face_rec.py
@@ -1,11 +1,11 @@
 import os
 import time
 import cv2
+from sklearn.metrics import precision_score, recall_score, f1_score
+
 from models.face_recognition.handler import OpenCVFaceRecognition
 from models.face_recognition.haar import HaarFrontModel, HaarProfileModel, HaarCombinedModel
 from models.face_recognition.resnet import ResNetModel
-import numpy as np
-from sklearn.metrics import precision_recall_curve, average_precision_score
 
 def get_correct_bounding_boxes(directory, text_file):
     bounding_boxes = {}
@@ -23,21 +23,6 @@ def get_correct_bounding_boxes(directory, text_file):
                         bounding_boxes[filename] = boxes
     return bounding_boxes
 
-def count_images_with_faces(face_recognition, directory):
-    count = 0
-    total = 0
-    faces_dict = {}
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            total += 1
-            img_path = os.path.join(directory, filename)
-            img = cv2.imread(img_path)
-            faces = face_recognition.model.detect_faces(img)
-            if len(faces) > 0:
-                count += 1
-                faces_dict[filename] = faces
-    return faces_dict, count, total
-
 def calculate_iou(box1, box2):
     x1, y1, w1, h1 = box1
     x2, y2, w2, h2 = box2
@@ -52,45 +37,62 @@ def calculate_iou(box1, box2):
     iou = inter_area / union_area
     return iou
 
-def calculate_precision_recall(predicted_boxes, correct_boxes):
-    tp = 0
-    fp = 0
-    fn = 0
+def get_classification_labels(predicted_boxes, correct_boxes):
+    y_true, y_pred = [], []
+
+    matched = set()
     for pred_box in predicted_boxes:
-        for correct_box in correct_boxes:
-            iou = calculate_iou(pred_box, correct_box)
-            if iou > 0.5:
-                tp += 1
-            else:
-                fp += 1
+        best_iou, best_match = max([(calculate_iou(pred_box, correct_box), idx) for idx, correct_box in enumerate(correct_boxes)], default=(0, -1))
+        if best_iou > 0.5 and best_match not in matched:
+            y_true.append(1)  # True Positive
+            matched.add(best_match)
+        else:
+            y_true.append(0)  # False Positive
+        y_pred.append(1)
+
+    # Add False Negatives
+    y_true.extend([1] * (len(correct_boxes) - len(matched)))
+    y_pred.extend([0] * (len(correct_boxes) - len(matched)))
+
+    return y_true, y_pred
+
+def calculate_metrics_for_image(predicted_boxes, correct_boxes):
+    tp = fp = fn = 0
+
+    matched = set()
+
+    for pred_box in predicted_boxes:
+        iou_scores = [calculate_iou(pred_box, correct_box) for correct_box in correct_boxes]
+        max_iou = max(iou_scores, default=0)
+        if max_iou > 0.5:
+            tp += 1
+            matched.add(iou_scores.index(max_iou))
         else:
             fp += 1
-    fn = len(correct_boxes) - tp
-    precision = tp / (tp + fp) if tp + fp > 0 else 0
-    recall = tp / (tp + fn) if tp + fn > 0 else 0
-    return precision, recall
 
-def calculate_average_precision(precision, recall):
-    precision = sorted(precision)
-    recall = sorted(recall)
-    prev_recall = 0
-    ap = 0
-    for p, r in zip(precision, recall):
-        ap += p * (r - prev_recall)
-        prev_recall = r
-    return ap
+    fn = len(correct_boxes) - len(matched)
+    return tp, fp, fn
 
-def calculate_map(faces_dict, bounding_boxes):
-    precisions = []
-    recalls = []
-    for filename in faces_dict:
-        predicted_boxes = faces_dict[filename]
-        correct_boxes = bounding_boxes[filename]
-        precision, recall = calculate_precision_recall(predicted_boxes, correct_boxes)
-        precisions.append(precision)
-        recalls.append(recall)
-    map = calculate_average_precision(precisions, recalls)
-    return map
+
+def evaluate_model(face_recognition, directory, bounding_boxes):
+    all_y_true, all_y_pred = [], []
+
+    for filename in os.listdir(directory):
+        if filename.endswith((".jpg", ".png")):
+            img_path = os.path.join(directory, filename)
+            img = cv2.imread(img_path)
+            predicted_boxes = face_recognition.model.detect_faces(img)
+            correct_boxes = bounding_boxes.get(filename, [])
+
+            y_true, y_pred = get_classification_labels(predicted_boxes, correct_boxes)
+            all_y_true.extend(y_true)
+            all_y_pred.extend(y_pred)
+
+    precision = precision_score(all_y_true, all_y_pred)
+    recall = recall_score(all_y_true, all_y_pred)
+    f1 = f1_score(all_y_true, all_y_pred)
+
+    return precision, recall, f1
 
 bounding_boxes = get_correct_bounding_boxes('./data/faces/', './data/wider_face_val_bbx_gt.txt')
 
@@ -112,17 +114,12 @@ other_dir = './data/other/'
 for model in models:
     print(f"Model: {type(model.model).__name__}")
     start_time = time.time()
-    faces_dict, faces_count, total_faces = count_images_with_faces(model, faces_dir)
 
-    
+    precision, recall, f1 = evaluate_model(model, faces_dir, bounding_boxes)
 
-    _, other_count, total_other = count_images_with_faces(model, other_dir)
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1-Score: {f1}")
+
     time_taken = time.time() - start_time
-
-
-    total_images = total_faces + total_other
-    ap = calculate_map(faces_dict, bounding_boxes)
-    print(f"Average Precision: {ap} for detecting faces in the 'faces' directory.")
-    print(f"Faces detected in {faces_count / total_faces * 100}% of images from the 'faces' directory.")
-    print(f"Faces detected in {other_count / total_other * 100}% of images from the 'other' directory.")
-    print(f"Time taken to test {total_images} images: {time_taken} seconds ({total_images/time_taken} images/second)")
+    print(f"Time taken: {time_taken} seconds")
